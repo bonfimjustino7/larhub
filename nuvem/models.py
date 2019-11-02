837ef57479331ed0016d307f28bf019f5d7f05eb
josircg <josircg@gmail.com>
2019-11-02 10:35:18 -0300

    Titulo do texto

--- nuvem/models.py
+++ nuvem/models.py
@@ -9,7 +9,7 @@ class Documento(models.Model):
     email = models.EmailField(max_length=50)
     arquivo = models.FileField('Arquivo em PDF ou Texto',upload_to='usuario_pdf', max_length=200)
     language = models.CharField('Linguagem', max_length=5, null=True, blank=True)
-    titulo = models.TextField('Título do artigo')
+    titulo = models.TextField('Título do artigo/livro', null=True, blank=True)git
 
     @property
     def texto(self):
